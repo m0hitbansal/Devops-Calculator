fede6fe7d33cd41290c306ec850278fa0ae73862
m0hitbansal <mohit.bansal@iiitb.org>
2020-05-28 14:28:21 +0530

    update

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -9,12 +9,12 @@ public class Calculator {
     }
 
     public static void main(String[] args) {
-	System.out.println("Welcome ");
+	System.out.println("Welcome to Dev- ops world");
         Calculator calculator = new Calculator();
         Scanner scanner = new Scanner(System.in);
         double number1=0.0, number2=0.0;
         do {
-            System.out.println("Devops-Calculator, Choose to perform operation");
+            System.out.println("Choose to perform operation");
             System.out.println("Press 1 to Add\nPress 2 to Subtract\nPress 3 to Multiply\nPress 4 to Divide\n" +
                     "Press any other key to exit\nEnter your choice: ");
             int choice = scanner.nextInt();
@@ -27,21 +27,21 @@ public class Calculator {
             switch (choice) {
                 case 1:
 
-                    System.out.println("Addition result is : " + calculator.add(number1, number2));
+                    System.out.println("Addition two number "+number1+ " and "+number2 +"  is : " + calculator.add(number1, number2));
                     break;
                 case 2:
-                    // do subtraction
+               
 
-                    System.out.println("Subtraction result is : " + calculator.subtract(number1, number2));
+                    System.out.println("Subtraction two number "+number1+ " and "+number2 +" is : " + calculator.subtract(number1, number2));
                     break;
                 case 3:
-                    // do multiplication
+                   
 
-                    System.out.println("Multiplication result is : " + calculator.multiply(number1, number2));
+                    System.out.println("Multiplication two number "+number1+ " and "+number2 +" is : " + calculator.multiply(number1, number2));
                     break;
                 case 4:
-                    // do division
-                    System.out.println("Multiplication result is : " + calculator.divide(number1, number2));
+                  
+                    System.out.println("Multiplication "+number1+ " and "+number2 +" is : " + calculator.divide(number1, number2));
                     break;
                 default:
                     System.out.println("Exiting....");
@@ -79,13 +79,13 @@ public class Calculator {
             logger.info("Dividing two numbers " + number1 + " and " + number2);
             if (number1 == 0 && number2 == 0) {
                 result = Double.NaN;
-                throw new ArithmeticException("Case of NaN 0.0/0.0");
+                throw new ArithmeticException("Case of  0.0/0.0");
             } else if (number1 > 0 && number2 == 0) {
                 result = Double.POSITIVE_INFINITY;
-                throw new ArithmeticException("Case of Positive Infinity 1.0/0.0");
+                throw new ArithmeticException("Case of  1.0/0.0");
             } else if (number1 <= -1 && number2 == 0) {
                 result = Double.NEGATIVE_INFINITY;
-                throw new ArithmeticException("Case of Negative Infinity -1.0/0.0");
+                throw new ArithmeticException("Case of  -1.0/0.0");
             } else {
                 result = number1 / number2;
             }
--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -8,54 +8,43 @@ public class CalculatorTest {
 
     @Test
     public void addingTruePositive(){
-        assertEquals("Adding two integer numbers for True Positive", 4, calculator.add(2, 2), DELTA);
-        assertEquals("Adding two double numbers for True Positive", 5.3, calculator.add(2.1, 3.2), DELTA);
+        assertEquals("Adding two numbers ", 50, calculator.add(47, 3), DELTA);
     }
 
     @Test
     public void addingFalsePositive(){
-        assertNotEquals("Adding two integer numbers for False Positive", 6, calculator.add(2, 2), DELTA);
-        assertNotEquals("Adding two double numbers for False Positive", 7.3, calculator.add(2.1, 3.2), DELTA);
+        assertNotEquals("Adding two  numbers ", 40.3, calculator.add(38.1, 28.2), DELTA);
     }
 
     @Test
     public void subtractingTruePositive(){
-        assertEquals("Subtracting two integer numbers for True Positive", 0, calculator.subtract(2, 2), DELTA);
-        assertEquals("Subtracting two double numbers for True Positive", -1.1, calculator.subtract(2.1, 3.2), DELTA);
-    }
-
-    @Test
-    public void subtractingFalsePositive(){
-        assertNotEquals("Subtracting two integer numbers for False Positive", 6, calculator.subtract(2, 2), DELTA);
-        assertNotEquals("Subtracting two double numbers for False Positive", -7.3, calculator.subtract(2.1, 3.2), DELTA);
+        assertEquals("Subtracting two r numbers ", 28, calculator.subtract(78, 50), DELTA);
+      
     }
 
     @Test
     public void multiplyingTruePositive(){
-        assertEquals("Multiplying two integer numbers for True Positive", 4, calculator.multiply(2, 2), DELTA);
-        assertEquals("Multiplying two double numbers for True Positive", 6.72, calculator.multiply(2.1, 3.2), DELTA);
+        assertEquals("Multiplying twor numbers ", 120, calculator.multiply(12, 10), DELTA);
+        
     }
 
     @Test
     public void multiplyingFalsePositive(){
-        assertNotEquals("Multiplying two integer numbers for False Positive", 6, calculator.multiply(2, 2), DELTA);
-        assertNotEquals("Multiplying two double numbers for False Positive", 7.3, calculator.multiply(2.1, 3.2), DELTA);
+        assertNotEquals("Multiplying two  numbers ", 8, calculator.multiply(2, 2), DELTA);
+       
     }
 
     @Test
     public void dividingTruePositive(){
-        assertEquals("Dividing two integer numbers for True Positive", 1, calculator.divide(2, 2), DELTA);
-        assertEquals("Dividing two double numbers for True Positive", Double.NaN, calculator.divide(0, 0), DELTA);
-        assertEquals("Dividing two double numbers for True Positive", Double.POSITIVE_INFINITY, calculator.divide(1, 0), DELTA);
-        assertEquals("Dividing two double numbers for True Positive", Double.NEGATIVE_INFINITY, calculator.divide(-1, 0), DELTA);
+        assertEquals("Dividing two  numbers ", 10, calculator.divide(100, 10), DELTA);
+        assertEquals("Dividing two  numbers ", Double.NaN, calculator.divide(0, 0), DELTA);
+       
     }
 
     @Test
     public void dividingFalsePositive(){
-        assertNotEquals("Dividing two integer numbers for True Positive", 1, calculator.divide(2.3, 1.3), DELTA);
-        assertNotEquals("Dividing two double numbers for True Positive", 0, calculator.divide(0, 0), DELTA);
-        assertNotEquals("Dividing two double numbers for True Positive", 1, calculator.divide(1, 0), DELTA);
-        assertNotEquals("Dividing two double numbers for True Positive", -1, calculator.divide(-1, 0), DELTA);
+        assertNotEquals("Dividing two  numbers", 1, calculator.divide(2.3, 1.3), DELTA);
+        
     }
 
 
